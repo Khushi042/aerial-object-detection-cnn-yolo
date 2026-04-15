f9b5843faa34daa53ac4aa0a3df645abfa2536c7
Khushi042 <khushi.udainia@gmail.com>
2026-04-15 18:59:24 +0530

    testing_yolo.py

--- testing_yolo.py
+++ testing_yolo.py
@@ -9,4 +9,4 @@ results = model("D:/project3/object_detection_Dataset/test/images/", show=True)
 # Save output
 results[0].save(filename="output.jpg")
 
-print("✅ Detection complete")
+print(" Detection complete")
